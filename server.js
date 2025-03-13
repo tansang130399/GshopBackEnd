dba69add927950b7f051272b7ed2ee4307e6ba9b
tansang130399 <tansang130399@gmail.com>
2025-03-13 14:18:36 +0700

    socket

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
-const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
+const io = new Server(server, {
   cors: {
-    origin: "*", // Cho phép mọi nguồn kết nối
+    origin: "*",
   },
 });
-
-app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  // Giả lập gửi thông báo từ server sau 5 giây
-  setTimeout(() => {
-    socket.emit("pushNotification", {
-      title: "Thông báo từ server",
-      message: "Bạn vừa nhận một thông báo mới!",
+  socket.on("requestNotification", () => {
+    io.emit("pushNotification", {
+      title: "Tiêu đề thông báo",
+      message: "Nội dung thông báo từ backend",
     });
-  }, 5000);
+  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server đang chạy trên cổng ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
